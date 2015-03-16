029cecb84e090d9d462e3a25462eb1ded8e4a634
CapraTheBest <gianluca.beladirr2y@gmail.com>
2015-03-16 22:08:48 +0100

    Better Test
    It shows that it's possible to takeover a computer with a few commands,
    by using the notepad :scream: .
    Also added a few comments about strange keys and for some functions on
    the code.

--- src/com/github/robot_test/fun/TestRobotKeys.java
+++ src/com/github/robot_test/fun/TestRobotKeys.java
@@ -15,16 +15,48 @@ public class TestRobotKeys {
     public TestRobotKeys() throws InterruptedException {
         try {
             robot = new Robot();
-            Thread.sleep(1000);
-            robot.setAutoDelay(250);
-            //robot.keyPress(KeyEvent.VK_ALT);
-            robot.keyPress(KeyEvent.VK_1);
-            robot.keyRelease(KeyEvent.VK_1);
-            robot.keyPress(KeyEvent.VK_2);
-            robot.keyRelease(KeyEvent.VK_2);
-            robot.keyPress(KeyEvent.VK_3);
-            robot.keyRelease(KeyEvent.VK_3);
-            //robot.keyRelease(KeyEvent.VK_ALT);
+            Thread.sleep(1000); 					//Random delay
+            robot.setAutoDelay(5); 					//Regulate text writing speed
+            robot.keyPress(KeyEvent.VK_WINDOWS); 	//Press the Windows Key to open start 
+            robot.keyPress(KeyEvent.VK_R);
+            robot.keyRelease(KeyEvent.VK_R);
+            robot.keyRelease(KeyEvent.VK_WINDOWS);
+            Thread.sleep(50); 						//Without this delay the code will write too fast and the pc won't recognize the "N"
+            robot.keyPress(KeyEvent.VK_N);
+            robot.keyRelease(KeyEvent.VK_N);
+            robot.keyPress(KeyEvent.VK_O);
+            robot.keyRelease(KeyEvent.VK_O);
+            robot.keyPress(KeyEvent.VK_T);
+            robot.keyRelease(KeyEvent.VK_T);
+            robot.keyPress(KeyEvent.VK_E);
+            robot.keyRelease(KeyEvent.VK_E);
+            robot.keyPress(KeyEvent.VK_P);
+            robot.keyRelease(KeyEvent.VK_P);
+            robot.keyPress(KeyEvent.VK_A);
+            robot.keyRelease(KeyEvent.VK_A);
+            robot.keyPress(KeyEvent.VK_D);
+            robot.keyRelease(KeyEvent.VK_D);
+            robot.keyPress(KeyEvent.VK_PERIOD); 	//It's the "." 
+            robot.keyRelease(KeyEvent.VK_PERIOD);
+            robot.keyPress(KeyEvent.VK_E);
+            robot.keyRelease(KeyEvent.VK_E);
+            robot.keyPress(KeyEvent.VK_X);
+            robot.keyRelease(KeyEvent.VK_X);
+            robot.keyPress(KeyEvent.VK_E);
+            robot.keyRelease(KeyEvent.VK_E);
+            robot.keyPress(KeyEvent.VK_ENTER); 		//The enter key, the one under the Delete key
+            robot.keyRelease(KeyEvent.VK_ENTER);
+            Thread.sleep(50); 						//Without this delay the code will write too fast 
+            robot.keyPress(KeyEvent.VK_P);			//and the pc won't recognize that's writing on the notepad
+            robot.keyRelease(KeyEvent.VK_P);
+            robot.keyPress(KeyEvent.VK_E);
+            robot.keyRelease(KeyEvent.VK_E);
+            robot.keyPress(KeyEvent.VK_N);
+            robot.keyRelease(KeyEvent.VK_N);
+            robot.keyPress(KeyEvent.VK_I);
+            robot.keyRelease(KeyEvent.VK_I);
+            robot.keyPress(KeyEvent.VK_S);
+            robot.keyRelease(KeyEvent.VK_S);
         } catch (AWTException ex) {
             ex.printStackTrace();
         }
